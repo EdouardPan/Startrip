d2959b72632e6dafb74f1fab157dbb5a425b9998
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-21 16:03:08 +0200

    [ADD] stars validation

--- app/models/star.rb
+++ app/models/star.rb
@@ -4,5 +4,11 @@ class Star < ApplicationRecord
   has_many :star_photos, dependent: :destroy
   has_many :star_reviews, dependent: :destroy
 
-  #ksjfhjlfdhfdjlhdfb
+  validates :name, presence: true, uniqueness :true
+  validates :constellation, presence: true,  inclusion: { in: ["Andromeda", "Antlia", "Apus"]} # Needs to be completed later
+  validates :water, inclusion: { in: (0..100).to_a }
+  validates :breathability, inclusion: { in: (0..100).to_a }
+  validates :asteroid_attack, numericality: { in: (0..100).to_a }
+  validates :description, presence: true, length: { minimum: 150 }
+  validates :price_per_day, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
