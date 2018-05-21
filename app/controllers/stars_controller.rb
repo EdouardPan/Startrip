3380f3e95adb14739080196527450419fe1cddb7
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-21 17:43:04 +0200

    [ADD] stars, star_reviews, and bookings controllers

---
+++ app/controllers/stars_controller.rb
@@ -0,0 +1,25 @@
+class StarsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def top
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'index/star_reviews'
+  get 'index/new'
+  get 'index/create'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/edit'
+  get 'bookings/update'
+  get 'bookings/destroy'
+  get 'stars/new'
+  get 'stars/create'
+  get 'stars/top'
+  get 'stars/edit'
+  get 'stars/update'
+  get 'stars/destroy'
+  get 'stars/show'
+  get 'stars/index'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
