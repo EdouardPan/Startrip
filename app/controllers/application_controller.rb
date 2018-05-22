e8e01ea5b6fa52afc91ebbfbc5c06f0e7415ed7c
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-22 12:47:55 +0200

    [ADD] policy added for stars.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  include Pundit
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Custumize in the controller
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -11,5 +19,10 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar, :human])
+  end
+
+  def skip_pundit?
+    # Don't touch this. This opens things up when there is no user.
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
