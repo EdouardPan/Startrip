07475261bd7984c08a97f24971cf9735682fff49
Floriane Perrin de Brichambaut <floperrindb@gmail.com>
2018-05-23 12:27:33 +0200

    [UPDATE] form show-page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@ import "bootstrap";
 
 import { loadDynamicBannerText } from '../components/banner';
 import {initUpdateNavbarOnScroll } from '../components/navbar';
+import "../plugins/flatpickr";
 
 loadDynamicBannerText();
 initUpdateNavbarOnScroll();
+
+
