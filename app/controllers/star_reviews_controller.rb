fdea8e8214c2922085eb9c55c52030d65c3802a5
Edouard Panossian <edouard.panossian@gmail.com>
2018-05-23 11:47:46 +0200

    [ADD] create method in star_reviews controller

--- app/controllers/star_reviews_controller.rb
+++ app/controllers/star_reviews_controller.rb
@@ -1,11 +1,25 @@
 class StarReviewsController < ApplicationController
-  def new
-  end
 
   def create
+    @star = Star.find(params[:star_id])
+    @star_review = StarReview.new(starreview_params)
+    @star_review.star = @star
+    if @star_review.save
+      redirect_to star_path(@star)
+    else
+      # Add instance variable of the stars#show once the controller stars finished
+      render 'stars/show'
+    end
   end
 
+  # To be deleted and integrated in the stars#show
   def index
     @star_reviews = policy_scope(StarReview)
+  end
+
+  private
+
+  def starreview_params
+    params.require(:star_review).permit(:rating, :comment)
   end
 end
