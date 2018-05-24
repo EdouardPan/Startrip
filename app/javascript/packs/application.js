6f18f649efcfe1d90a294dce5001bb0580e152b4
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-24 12:28:44 +0200

    trying to make this fucking slider

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,10 @@ import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 import {initUpdateNavbarOnScroll } from '../components/navbar';
 import "../plugins/flatpickr";
+import { PrettySlider } from '..components/slider'
 
 loadDynamicBannerText();
 initUpdateNavbarOnScroll();
+PrettySlider();
 
 
